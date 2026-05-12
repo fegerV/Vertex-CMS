89fee8a8bb202433f147d60ad551225f68f670f4
Feger Viktor <fegervic@gmail.com>
2026-05-12 17:53:18 +0500

    fix: add web+auth middleware to media API routes for session authentication

--- routes/api.php
+++ routes/api.php
@@ -14,17 +14,20 @@ Route::get('/pages/{page}', [PageApiController::class, 'show']);
 Route::put('/pages/{page}', [PageApiController::class, 'update']);
 Route::delete('/pages/{page}', [PageApiController::class, 'destroy']);
 
- Route::get('/media', [MediaApiController::class, 'index']);
- Route::post('/media/upload', [MediaApiController::class, 'store']);
- Route::put('/media/{media}', [MediaApiController::class, 'update']);
- Route::patch('/media/{media}/move', [MediaApiController::class, 'move']);
- Route::delete('/media/{media}', [MediaApiController::class, 'destroy']);
+ // Media & Folders (admin panel uses session auth)
+ Route::middleware(['web', 'auth'])->group(function (): void {
+     Route::get('/media', [MediaApiController::class, 'index']);
+     Route::post('/media/upload', [MediaApiController::class, 'store']);
+     Route::put('/media/{media}', [MediaApiController::class, 'update']);
+     Route::patch('/media/{media}/move', [MediaApiController::class, 'move']);
+     Route::delete('/media/{media}', [MediaApiController::class, 'destroy']);
 
- // Media Folders
- Route::get('/media/folders', [MediaFolderApiController::class, 'index']);
- Route::post('/media/folders', [MediaFolderApiController::class, 'store']);
- Route::put('/media/folders/{folder}', [MediaFolderApiController::class, 'update']);
- Route::delete('/media/folders/{folder}', [MediaFolderApiController::class, 'destroy']);
+     // Media Folders
+     Route::get('/media/folders', [MediaFolderApiController::class, 'index']);
+     Route::post('/media/folders', [MediaFolderApiController::class, 'store']);
+     Route::put('/media/folders/{folder}', [MediaFolderApiController::class, 'update']);
+     Route::delete('/media/folders/{folder}', [MediaFolderApiController::class, 'destroy']);
+ });
 
 Route::get('/builder/blocks', [BuilderApiController::class, 'blocks']);
 Route::post('/builder/render-preview', [BuilderApiController::class, 'renderPreview']);
